ba4b5383301a1ce67151612081c978a58cbed557
joeisble <joeisble@yahoo.com>
2018-08-04 17:16:09 -0400

    start of angular project

--- Cheapware-app/src/app/app.module.ts
+++ Cheapware-app/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { RouterModule } from '../../node_modules/@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
